8021841424acc4da731ce5d5888bf5032068d5a4
Thanatat Tamtan <acoshift@gmail.com>
2022-05-31 21:03:54 +0700

    mirror at 20220531210349

--- api/deploymentaction.go
+++ api/deploymentaction.go
@@ -1,5 +1,7 @@
 package api
 
+import "encoding/json"
+
 //go:generate stringer -type=DeploymentAction -linecomment
 type DeploymentAction int
 
@@ -9,3 +11,25 @@ const (
 	DeploymentActionDelete                  // delete
 	DeploymentActionPause                   // pause
 )
+
+func (a DeploymentAction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(a.String())
+}
+
+func (a *DeploymentAction) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+
+	*a = DeploymentAction(0)
+
+	for _, x := range []DeploymentAction{DeploymentActionDeploy, DeploymentActionDelete, DeploymentActionPause} {
+		if x.String() == s {
+			*a = x
+			return nil
+		}
+	}
+	return nil
+}
